b01f26e1968d6ee1c3295f3fa23cb30f22b2fef5
Peter Knipp <peteraknipp@gmail.com>
2024-07-31 07:50:24 -0400

    Grab routeParams using getAttribute function.

--- web/index.php
+++ web/index.php
@@ -41,8 +41,10 @@ $app->get('/json', function(Request $request, Response $response, LoggerInterfac
   return $twig->render($response, 'json.twig');
 });
 
-$app->get('/capture/{data}', function(Request $request, Response $response, $args) {
-  $data = $args['data'];
+$app->get('/capture/{data}', function(Request $request, Response $response, ContainerInterface $container)
+{
+  $routeParams = $request->getAttribute('routeParams');
+  $data = $routeParams['data'];
   return $response->withJson(['data' => $data]);
 });
 
